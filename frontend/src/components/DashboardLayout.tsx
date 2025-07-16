5fbb00e247913a0e86dccab62faee4fe1715b18c
Jon <jprietomdev@gmail.com>
2025-07-16 14:41:46 +0200

    Mejora el diseño del panel de control de Gym Track. Se ajustan los estilos de las tarjetas del dashboard, se añaden márgenes y padding, y se implementan estilos responsivos. Se actualiza la lógica de autenticación en el layout del dashboard para manejar el cierre de sesión y la verificación del usuario. Se optimizan los elementos de navegación y se mejora la estructura del código.

--- frontend/src/components/DashboardLayout.tsx
+++ frontend/src/components/DashboardLayout.tsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import { ArrowRightStartOnRectangleIcon } from '@heroicons/react/24/outline';
 import './DashboardLayout.css';
+import { useAuth } from '@/context/AuthContext';
 
 interface DashboardLayoutProps {
   children: ReactNode;
@@ -12,17 +13,16 @@ interface DashboardLayoutProps {
 
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
   const router = useRouter();
+  const { logout, user, loading } = useAuth();
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (!token) {
+    if (!loading && !user) {
       router.push('/login');
     }
-  }, [router]);
+  }, [user, loading, router]);
 
   const handleLogout = () => {
-    localStorage.removeItem('token');
-    router.push('/login');
+    logout();
   };
 
   return (
@@ -30,12 +30,19 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
       <nav className="nav-header">
         <div className="max-w-7xl mx-auto nav-container">
           <div className="w-full flex flex-col">
-            <div className="w-full flex items-center justify-between mb-4">
+            <div className="header-top">
               <Link href="/dashboard" className="nav-logo">
                 Gym Track
               </Link>
+              <button 
+                onClick={handleLogout} 
+                className="logout-icon-button"
+                title="Cerrar Sesión"
+              >
+                <ArrowRightStartOnRectangleIcon className="h-6 w-6 text-white" />
+              </button>
             </div>
-            <div className="flex items-center justify-between">
+            <div className="nav-links-container">
               <div className="flex items-center space-x-6">
                 <Link href="/dashboard" className="nav-link">
                   Dashboard
@@ -46,15 +53,7 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
                 <Link href="/progress" className="nav-link">
                   Progreso
                 </Link>
-                 <button 
-                onClick={handleLogout} 
-                className="logout-icon-button"
-                title="Cerrar Sesión"
-              >
-                <ArrowRightStartOnRectangleIcon className="h-6 w-6 text-white" />
-              </button>
               </div>
-             
             </div>
           </div>
         </div>
